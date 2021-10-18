c76c16a4fd87edb52a71637a2806d9cd28979e3c
unknown <andi.ariefpkp89@gmail.com>
2021-10-18 11:09:18 +0700

    Update dari Kantor 18102021_01

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('testing', function() {
+    return "Hello World";
+});
+
+Route::resource('post', PostController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
