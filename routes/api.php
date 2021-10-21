7d441ccb91f80dff4f3c48f31510dc10b264113f
unknown <andi.ariefpkp89@gmail.com>
2021-10-21 14:33:23 +0700

    Update dari Kantor 21102021_01

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,14 +17,11 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('testing', function() {
-    return "Hello World";
-});
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name("login");
 
-Route::resource('post', PostController::class)->except([
-    'create', 'edit'
-]);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('post', PostController::class)->except([
+        'create', 'edit'
+    ]);
 });
